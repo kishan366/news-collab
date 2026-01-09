714757fc2aadef115b1a077aabcd9953753d0e96
savanthakrar96 <savanthakarar41@gmail.com>
2026-01-09 15:04:43 +0530

    Add the apis

---
+++ server/src/models/User.js
@@ -0,0 +1,24 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define("User", {
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+export default User;
